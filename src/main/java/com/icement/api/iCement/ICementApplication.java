fa145cc10330a965cb20cef6847ecb5adb242801
codsky <brilliantwise@gmail.com>
2025-03-24 16:00:30 +0100

    initial repo setup

---
+++ src/main/java/com/icement/api/iCement/ICementApplication.java
@@ -0,0 +1,13 @@
+package com.icement.api.iCement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icement/api/iCement/ICementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icement.api.iCement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
